feb483ab901e64e7bb9f961b0fd8787ed492b738
Ziyoiddin Yusupov <ziyoiddin.yusupov@gmail.com>
2017-03-14 03:37:58 +0300

    added my own network, forpot for first time )))

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 docker push yusupov/sample-node
 
-ssh yusupovtestmail@104.199.107.164
+#ssh yusupovtestmail@104.199.107.164
 docker pull yusupov/sample-node:latest
 docker stop web || true
 docker rm web || true
 docker rmi yusupov/sample-node:current || true
 docker tag yusupov/sample-node:latest yusupov/sample-node:current
-docker run -d --net app --restart always --name web -p 80:80 yusupov/sample-node:current
+docker run -d --net my-network --restart always --name web -p 80:80 yusupov/sample-node:current
